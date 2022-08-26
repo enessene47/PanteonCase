aa1e207e162986419e6852185afe0bb6cd6b133a
enessene47 <enes.sene.47@gmail.com>
2022-08-26 03:20:56 +0300

    Update

--- Assets/Scripts/Singleton/MonoSingleton.cs
+++ Assets/Scripts/Singleton/MonoSingleton.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-public class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
+public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
 {
     private static volatile T _instance = null;
 
